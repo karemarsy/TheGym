b9fb079ca58275962aaf509958a3d669f02fd1e3
Arsene <arsene@Arsenes-MacBook-Pro-2.local>
2023-05-30 13:00:21 +0200

    Solve exercise 3

--- exercise.js
+++ exercise.js
@@ -1,7 +1,13 @@
-function createPhoneNumber(numbers){
-  var numberStr = numbers.join('');
+function createPhoneNumber(numbers) {
+  var numberStr = numbers.join("");
 
-  var formattedNumber = '(' + numberStr.substring(0, 3) + ') ' + numberStr.substring(3, 6) + '-' + numberStr.substring(6);
-  
+  var formattedNumber =
+    "(" +
+    numberStr.substring(0, 3) +
+    ") " +
+    numberStr.substring(3, 6) +
+    "-" +
+    numberStr.substring(6);
+
   return formattedNumber;
 }
